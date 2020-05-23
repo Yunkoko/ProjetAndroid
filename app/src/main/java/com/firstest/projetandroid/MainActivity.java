b4edb4f656a43300d660986329f1f3f5c16814c4
Yun <jayzyun@gmail.com>
2020-05-23 16:22:09 +0200

    end td5 : cache data

--- app/src/main/java/com/firstest/projetandroid/MainActivity.java
+++ app/src/main/java/com/firstest/projetandroid/MainActivity.java
@@ -1,15 +1,20 @@
 package com.firstest.projetandroid;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.widget.Toast;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import com.google.gson.reflect.TypeToken;
 
+import java.lang.reflect.Type;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -27,6 +32,8 @@ public class MainActivity extends AppCompatActivity {
     private ListAdapter mAdapter;
     private RecyclerView.LayoutManager layoutManager;
     private static final String BASE_URL = "https://ghibliapi.herokuapp.com/";
+    private SharedPreferences sharedPreferences;
+    private Gson gson;
 
 
     @Override
@@ -34,7 +41,29 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        makeApiCall();
+        sharedPreferences = getSharedPreferences("app_project", Context.MODE_PRIVATE);
+
+        gson = new GsonBuilder()
+                .setLenient()
+                .create();
+
+        List<GMovies> GMoviesList = getDataFromCache();
+        if(GMoviesList != null) {
+            showList(GMoviesList);
+        } else {
+            makeApiCall();
+        }
+    }
+
+    private List<GMovies> getDataFromCache() {
+        String jsonGmovies = sharedPreferences.getString("jsonGMoviesList", null);
+
+        if(jsonGmovies == null){
+            return null;
+        } else {
+            Type listType = new TypeToken<List<GMovies>>(){}.getType();
+            return gson.fromJson(jsonGmovies, listType);
+        }
     }
 
     private void showList(List<GMovies> GMoviesList) {
@@ -50,9 +79,6 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void makeApiCall() {
-        Gson gson = new GsonBuilder()
-                .setLenient()
-                .create();
 
         Retrofit retrofit = new Retrofit.Builder()
                 .baseUrl(BASE_URL)
@@ -64,10 +90,11 @@ public class MainActivity extends AppCompatActivity {
         Call<List<GMovies>> call = MovieApi.getMovieResponse();
         call.enqueue(new Callback<List<GMovies>>() {
             @Override
-            public void onResponse(Call<List<GMovies>> call, Response<List<GMovies>> response) {
+            public void onResponse(@NonNull Call<List<GMovies>> call, @NonNull Response<List<GMovies>> response) {
                 if(response.isSuccessful() && response.body() != null) {
                     List<GMovies> GMovieList = response.body();
                     showList(GMovieList);
+                    savelist(GMovieList);
                 } else {
                     showError();
                 }
@@ -78,6 +105,17 @@ public class MainActivity extends AppCompatActivity {
                 showError();
             }
         });
+    }
+
+    private void savelist(List<GMovies> gMovieList) {
+        String jsonString = gson.toJson(gMovieList);
+        sharedPreferences
+                .edit()
+                .putString("jsonGMoviesList", jsonString)
+                .apply();
+
+        Toast.makeText(getApplicationContext(), "List saved", Toast.LENGTH_SHORT).show();
+
     }
 
     private void showError() {
